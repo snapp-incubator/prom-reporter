a136c5462a61d0b0fcd29152b3d3f93692d3e695
Hamed Karbasi <hamed.karbasi.93@gmail.com>
2024-02-29 00:19:15 +0330

    fix empty results by returning dataframe with a null value

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,19 +1,24 @@
 from unittest.mock import patch, Mock
 from app.main import main
 
+
 @patch(
     "app.helper.prometheus.PrometheusConnect.check_prometheus_connection",
     Mock(return_value=True),
 )
 @patch(
     "app.helper.prometheus.PrometheusConnect.custom_query",
+    # It uses two outputs for the prometheus: an ordinary and an empty result.
     Mock(
-        return_value=[
-            {
-                "metric": {"__name__": "dummy_metric"},
-                "value": [1708184035.052, "0.29875"],
-            }
-        ]
+        side_effect=[
+            [
+                {
+                    "metric": {"__name__": "dummy_metric"},
+                    "value": [1708184035.052, "0.29875"],
+                }
+            ],
+            [],
+        ],
     ),
 )
 def test_main():
